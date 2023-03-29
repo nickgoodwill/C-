06a59cbaf66e9c0c90ba425f065452e1479584d8
Nick Goodwill <nick.goodwill@yandex.ru>
2023-03-29 22:11:18 +0300

    added forth task

--- C#HW1/Program.cs
+++ C#HW1/Program.cs
@@ -1,60 +1,60 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+﻿//2// Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
 
-//int min = 0;
-//int max = 0;
-//Console.Write("Введите первое число: ");
-//int a = int.Parse(Console.ReadLine());
-//Console.Write("Введите второе число: ");
-//int b = int.Parse(Console.ReadLine());
+int min = 0;
+int max = 0;
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
 
-//if (a > b)
-//{
-//    max = a;
-//    min = b;
-//    Console.WriteLine("Первое число больше второго");
-//    Console.Write(max);Console.Write(", "); Console.Write(min);
-//}
-//else
-//{
-//    max = b;
-//    min = a;
-//    Console.WriteLine("Второе число больше первого");
-//    Console.Write(max); Console.Write(", "); Console.Write(min);
-//}
+if (a > b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine("Первое число больше второго");
+    Console.Write(max);Console.Write(", "); Console.Write(min);
+}
+else
+{
+    max = b;
+    min = a;
+    Console.WriteLine("Второе число больше первого");
+    Console.Write(max); Console.Write(", "); Console.Write(min);
+}
 
-// Напишите программу, которая принимает на вход три числа 
+//4// Напишите программу, которая принимает на вход три числа 
 // и выдаёт максимальное из этих чисел.
 
-//int max = 0;
-//Console.Write("Введите первое число: ");
-//int a = int.Parse(Console.ReadLine());
-//Console.Write("Введите второе число: ");
-//int b = int.Parse(Console.ReadLine());
-//Console.Write("Введите третье число: ");
-//int c = int.Parse(Console.ReadLine());
+int max = 0;
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
 
-//if (a > b)
-//{
-//    max = a;
-//}
-//else
-//{
-//    max = b;
-//}
-//if (max > c)
-//{
-//    Console.WriteLine("Максимальное число: ");
-//    Console.Write(max);
-//}
-//else
-//{
-//    max = c;
-//    Console.WriteLine("Максимальное число: ");
-//    Console.Write(max);
-//}
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.Write(max);
+}
+else
+{
+    max = c;
+    Console.WriteLine("Максимальное число: ");
+    Console.Write(max);
+}
 
-// Напишите программу, которая на вход принимает число и выдаёт, 
+//6// Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка).
 
 Console.Write("Введите число: ");
@@ -69,4 +69,22 @@ else
 {
     Console.Write(a);
     Console.WriteLine(" - Нечетное число");
+}
+
+//8// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 1)
+{
+    for (int res = 2; res <= n; res = res + 2)
+    {
+            Console.Write(res); Console.Write(" ");  
+    }
+}
+else
+{
+    Console.WriteLine("Нет четных чисел");
 }
